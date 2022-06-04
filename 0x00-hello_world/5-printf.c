801af14eb11992ca672dd75ccaeaac16c690b828
Edisonpryce <edisonpryce@gmail.com>
2022-06-04 06:10:03 -0700

    Hello printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,18 +2,14 @@
 
 /**
  *
- *  * Description: main - Prints out a string to stdout.
+ * main - Prints out a string to stdout.
  *
- *   * Return: 0 if success.
+ * Return: 0 if success.
  *
- *    */
+ */
 
 int main(void)
-
 {
-
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-
-	return (0);
-
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
