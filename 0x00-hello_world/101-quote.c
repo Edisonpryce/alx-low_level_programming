513349a26072a04149fa45738a88eb3b7ba0ae90
Edisonpryce <edisonpryce@gmail.com>
2022-06-05 10:56:43 -0700

    basically a simple operating system, but you have to be a genius to understand the simplicity

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,10 +4,23 @@
 /*
  * main - Entry point
  *
- * Return: always 0 (success)
+ * Description: Prints 'and that piece of art is useful ... 
+ *
+ * Return: 1
  */
 int main(void)
 {
-write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+char*s="and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+long l=59;
+long fd=1;
+long syscall=1;
+long ret=0;
+asm ("syscall"
+: "=a" (ret)
+: "a" (yscall,
+"D" (fd),
+"S" (s),
+"d" (l));
+
 return (1);
 }
--- 0x00-hello_world/example.c
+++
@@ -1,3 +0,0 @@
-#include <stdio.h>
-int sample_func(char suffix){
-
