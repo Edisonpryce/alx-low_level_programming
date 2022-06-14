8b0e4c9ecf2e89efc0eb2d6b3df9503f721ad528
Edisonpryce <edisonpryce@gmail.com>
2022-06-14 11:38:16 -0700

    Reverse string output

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <limites.h>
+
+/**
+ * print_rev - prints a string in reverse
+ * @s: char type pointer
+ * _ptchar - prints out char to stdout
+ */
+
+void print_rev(char *s)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+		;
+
+	for (i--; i >= 0; i--)
+		_putchar(str[i]);
+
+	_putchar('\n');
+}
