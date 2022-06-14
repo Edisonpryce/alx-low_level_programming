097ae0b067cddee75196e21c6e06de13eab7575c
Edisonpryce <edisonpryce@gmail.com>
2022-06-14 05:58:16 -0700

    Swapping integers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * swap_int - pointer prom swapping pointer int values
+ * @a: pointer int value
+ * @b: point int value
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
